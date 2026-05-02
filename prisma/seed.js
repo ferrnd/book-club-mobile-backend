c62a1b15c9ee809228fda37e5e80ddc9ce6f66ee
ferrnd <sombarepam@gmail.com>
2026-05-02 18:51:57 -0300

    feat: adiciona inserção de membros no seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -867,6 +867,15 @@ async function main() {
         linkedin: "",
         email: "",
       },
+      {
+        nome: "Cauã Tupinambá",
+        curso_pt: "Desenvolvimento de Sistemas",
+        curso_en: "Systems Development",
+        fotoUrl: "",
+        github: "",
+        linkedin: "",
+        email: "",
+      },
     ],
   });
 
