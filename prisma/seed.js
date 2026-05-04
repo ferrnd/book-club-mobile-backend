7ff44b2c8ad5b391148833ce4649b21e4514acd8
ferrnd <sombarepam@gmail.com>
2026-05-04 09:34:09 -0300

    feat: adiciona dica no seed.js sobre 'Possíveis temas de redação sobre o livro principal'

--- prisma/seed.js
+++ prisma/seed.js
@@ -858,6 +858,12 @@ async function main() {
                 conteudo_pt: 'Pobreza e desigualdade social',
                 conteudo_en: 'Poverty and social inequality',
             },
+            {
+                tipo_pt: 'Possíveis temas de redação sobre o livro principal',
+                tipo_en: 'Potential essay topics for the main book',
+                conteudo_pt: 'Memória e ancestralidade como resistência',
+                conteudo_en: 'Memory and ancestry as forms of resistance',
+            },
         ],
     });
 
