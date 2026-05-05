beadaa5d122d32238715bc4b11dfb2593c4b7dd3
ferrnd <sombarepam@gmail.com>
2026-05-05 18:16:52 -0300

    feat: cria rota para gerenciamento de autores com métodos de busca e atualização

---
+++ src/routes/autorRoute.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as controller from '../controllers/autorController.js';
+
+const router = express.Router();
+
+router.get('/autor', controller.buscarTodos);
+router.get('/autor/:id', controller.buscarPorId);
+router.put('/autor/:id', controller.atualizar);
+
+export default router;
