1c742c9c35e58e0c30d4b723aa621349fc0035ed
ferrnd <sombarepam@gmail.com>
2026-05-05 17:21:17 -0300

    feat: adiciona importação e uso das rotas de projeto no servidor

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import livrosRoutes from './routes/livroRoute.js';
+import projetoRoutes from './routes/projetoRoute.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/', livrosRoutes);
+app.use('/', projetoRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Rota não encontrada' });
