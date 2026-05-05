0b844aca5537b5634e287bdc108a96972b588e43
ferrnd <sombarepam@gmail.com>
2026-05-05 16:16:58 -0300

    feat: faz rotas de livros, removendo as não necessarias "post" e "delete"

--- src/routes/livroRoute.js
+++ src/routes/livroRoute.js
@@ -3,10 +3,8 @@ import * as controller from '../controllers/livroController.js';
 
 const router = express.Router();
 
-router.post('/livro', controller.criar);
 router.get('/livro', controller.buscarTodos);
 router.get('/livro/:id', controller.buscarPorId);
 router.put('/livro/:id', controller.atualizar);
-router.delete('/livro/:id', controller.deletar);
 
 export default router;
