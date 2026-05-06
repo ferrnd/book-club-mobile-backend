bcc1fe0c316551a1327110e3c008ac86c5e7634e
ferrnd <sombarepam@gmail.com>
2026-05-06 18:12:10 -0300

    feat: cria rota para gerenciamento de contos com métodos de busca e atualização

---
+++ src/routes/contoRoute.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as controller from '../controllers/contoController.js';
+
+const router = express.Router();
+
+router.get('/contos', controller.buscarTodos);
+router.get('/contos/:id', controller.buscarPorId);
+router.put('/contos/:id', controller.atualizar);
+
+export default router;
