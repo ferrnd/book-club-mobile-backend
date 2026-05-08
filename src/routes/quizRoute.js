b720e78bf2f6e7b84be11634ea1bc2e8572fd4f9
ferrnd <sombarepam@gmail.com>
2026-05-08 14:57:19 -0300

    feat: adiciona routes de quiz

---
+++ src/routes/quizRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import * as controller from '../controllers/quizController.js';
+
+const router = express.Router();
+
+router.post('/quiz', controller.criar);
+router.get('/quiz', controller.buscarTodos);
+router.get('/quiz/:id', controller.buscarPorId);
+router.put('/quiz/:id', controller.atualizar);
+router.delete('/quiz/:id', controller.deletar);
+
+export default router;
