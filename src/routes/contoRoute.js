a89847cfa1b87b0fba5b9c6769b043d6054e2e22
ferrnd <sombarepam@gmail.com>
2026-05-08 19:19:40 -0300

    style: formata contoRoute.js com prettier

--- src/routes/contoRoute.js
+++ src/routes/contoRoute.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import * as controller from '../controllers/contoController.js';
+import express from "express";
+import * as controller from "../controllers/contoController.js";
 
 const router = express.Router();
 
-router.get('/contos', controller.buscarTodos);
-router.get('/contos/:id', controller.buscarPorId);
-router.put('/contos/:id', controller.atualizar);
+router.get("/contos", controller.buscarTodos);
+router.get("/contos/:id", controller.buscarPorId);
+router.put("/contos/:id", controller.atualizar);
 
 export default router;
