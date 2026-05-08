bad6b4fc7c0f3d652935efa2672976c0f50f59c9
ferrnd <sombarepam@gmail.com>
2026-05-08 19:19:58 -0300

    style: formata server.js com prettier

--- src/server.js
+++ src/server.js
@@ -1,40 +1,40 @@
-import express from 'express';
-import 'dotenv/config';
-import livrosRoutes from './routes/livroRoute.js';
-import projetoRoutes from './routes/projetoRoute.js';
-import autorRoutes from './routes/autorRoute.js';
-import contoRoutes from './routes/contoRoute.js';
-import membroRoutes from './routes/membroRoute.js';
-import personagemRoutes from './routes/personagemRoute.js'
-import citacaoRoutes from './routes/citacaoRoute.js'
-import dicaRoutes from './routes/dicaRoute.js'
-import quizRoutes from './routes/quizRoute.js'
-import videoaulaRoute from './routes/videoaulaRoute.js';
+import express from "express";
+import "dotenv/config";
+import livrosRoutes from "./routes/livroRoute.js";
+import projetoRoutes from "./routes/projetoRoute.js";
+import autorRoutes from "./routes/autorRoute.js";
+import contoRoutes from "./routes/contoRoute.js";
+import membroRoutes from "./routes/membroRoute.js";
+import personagemRoutes from "./routes/personagemRoute.js";
+import citacaoRoutes from "./routes/citacaoRoute.js";
+import dicaRoutes from "./routes/dicaRoute.js";
+import quizRoutes from "./routes/quizRoute.js";
+import videoaulaRoute from "./routes/videoaulaRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('🚀 API funcionando');
+app.get("/", (req, res) => {
+  res.send("🚀 API funcionando");
 });
 
-app.use('/', livrosRoutes);
-app.use('/', projetoRoutes);
-app.use('/', autorRoutes);
-app.use('/', contoRoutes);
-app.use('/', membroRoutes);
-app.use('/', personagemRoutes);
-app.use('/', citacaoRoutes);
-app.use('/', dicaRoutes);
-app.use('/', quizRoutes);
-app.use('/', videoaulaRoute);
+app.use("/", livrosRoutes);
+app.use("/", projetoRoutes);
+app.use("/", autorRoutes);
+app.use("/", contoRoutes);
+app.use("/", membroRoutes);
+app.use("/", personagemRoutes);
+app.use("/", citacaoRoutes);
+app.use("/", dicaRoutes);
+app.use("/", quizRoutes);
+app.use("/", videoaulaRoute);
 
 app.use((req, res) => {
-    res.status(404).json({ error: 'Rota não encontrada' });
+  res.status(404).json({ error: "Rota não encontrada" });
 });
 
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
 });
