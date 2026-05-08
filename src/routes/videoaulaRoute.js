08d3eaa886e61500093fb4bad37cd7f47dba3ff1
ferrnd <sombarepam@gmail.com>
2026-05-08 19:19:47 -0300

    style: formata videoaulaRoute.js com prettier

--- src/routes/videoaulaRoute.js
+++ src/routes/videoaulaRoute.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import * as controller from '../controllers/videoaulaController.js';
+import express from "express";
+import * as controller from "../controllers/videoaulaController.js";
 
 const router = express.Router();
 
-router.get('/videoaula', controller.buscarTodos);
-router.get('/videoaula/:id', controller.buscarPorId);
-router.put('/videoaula/:id', controller.atualizar);
+router.get("/videoaula", controller.buscarTodos);
+router.get("/videoaula/:id", controller.buscarPorId);
+router.put("/videoaula/:id", controller.atualizar);
 
 export default router;
