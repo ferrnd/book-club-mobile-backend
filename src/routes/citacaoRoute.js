5cc51bd22bf995c4479647e9fe4289e16836eef5
ferrnd <sombarepam@gmail.com>
2026-05-08 19:19:38 -0300

    style: formata citacaoRoute.js com prettier

--- src/routes/citacaoRoute.js
+++ src/routes/citacaoRoute.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import * as controller from '../controllers/citacaoController.js';
+import express from "express";
+import * as controller from "../controllers/citacaoController.js";
 
 const router = express.Router();
 
-router.get('/citacao', controller.buscarTodos);
-router.get('/citacao/:id', controller.buscarPorId);
-router.put('/citacao/:id', controller.atualizar);
+router.get("/citacao", controller.buscarTodos);
+router.get("/citacao/:id", controller.buscarPorId);
+router.put("/citacao/:id", controller.atualizar);
 
 export default router;
