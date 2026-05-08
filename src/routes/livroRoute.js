7cbdab3df920c55662b0c64cb5a816fe952d58b2
ferrnd <sombarepam@gmail.com>
2026-05-08 19:19:42 -0300

    style: formata livroRoute.js com prettier

--- src/routes/livroRoute.js
+++ src/routes/livroRoute.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import * as controller from '../controllers/livroController.js';
+import express from "express";
+import * as controller from "../controllers/livroController.js";
 
 const router = express.Router();
 
-router.get('/livro', controller.buscarTodos);
-router.get('/livro/:id', controller.buscarPorId);
-router.put('/livro/:id', controller.atualizar);
+router.get("/livro", controller.buscarTodos);
+router.get("/livro/:id", controller.buscarPorId);
+router.put("/livro/:id", controller.atualizar);
 
 export default router;
