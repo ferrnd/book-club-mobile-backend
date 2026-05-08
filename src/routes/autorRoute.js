579eb89ccf6e095e2e64714c0714273fd84317ac
ferrnd <sombarepam@gmail.com>
2026-05-08 19:19:38 -0300

    style: formata autorRoute.js com prettier

--- src/routes/autorRoute.js
+++ src/routes/autorRoute.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import * as controller from '../controllers/autorController.js';
+import express from "express";
+import * as controller from "../controllers/autorController.js";
 
 const router = express.Router();
 
-router.get('/autor', controller.buscarTodos);
-router.get('/autor/:id', controller.buscarPorId);
-router.put('/autor/:id', controller.atualizar);
+router.get("/autor", controller.buscarTodos);
+router.get("/autor/:id", controller.buscarPorId);
+router.put("/autor/:id", controller.atualizar);
 
 export default router;
