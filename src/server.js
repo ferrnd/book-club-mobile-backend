41ede9c1c093fd781733917daeed7623c32b9bc6
mariaeandrade <dudadeandrade13@gmail.com>
2026-05-08 14:28:39 -0300

    feat: adiciona CRUD completo de citação

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import projetoRoutes from './routes/projetoRoute.js';
 import autorRoutes from './routes/autorRoute.js';
 import contoRoutes from './routes/contoRoute.js';
 import membroRoutes from './routes/membroRoute.js';
+import citacaoRoutes from './routes/citacaoRoute.js'
+import dicaRoutes from './routes/dicaRoute.js'
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,6 +22,8 @@ app.use('/', projetoRoutes);
 app.use('/', autorRoutes);
 app.use('/', contoRoutes);
 app.use('/', membroRoutes);
+app.use('/', citacaoRoutes);
+app.use('/', dicaRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Rota não encontrada' });
