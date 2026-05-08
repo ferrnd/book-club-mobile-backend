6ee9f8511f8df9581e2122a1b914fcfed41e78ab
ferrnd <sombarepam@gmail.com>
2026-05-08 19:19:44 -0300

    style: formata personagemRoute.js com prettier

--- src/routes/personagemRoute.js
+++ src/routes/personagemRoute.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import * as controller from '../controllers/personagemController.js';
+import express from "express";
+import * as controller from "../controllers/personagemController.js";
 
 const router = express.Router();
 
-router.post('/personagens', controller.criar);
-router.get('/personagens', controller.buscarTodos);
-router.get('/personagens/:id', controller.buscarPorId);
-router.put('/personagens/:id', controller.atualizar);
-router.delete('/personagens/:id', controller.deletar);
+router.post("/personagens", controller.criar);
+router.get("/personagens", controller.buscarTodos);
+router.get("/personagens/:id", controller.buscarPorId);
+router.put("/personagens/:id", controller.atualizar);
+router.delete("/personagens/:id", controller.deletar);
 
 export default router;
