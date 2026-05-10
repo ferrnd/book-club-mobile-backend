8f07dcbe4606946213ebf6ec1971983a00bddab4
ferrnd <sombarepam@gmail.com>
2026-05-10 13:30:52 -0300

    feat: faz admKey no middleware para que apenas eu possa ter acesso a edição de outras tabelas
    - quem tiver a apiKey apenas consegue o GET de livro e não consegue ter acesso a mais nenhuma tabela
    
    -  quem tiver a admKey consegue ter acesso a edição completa de todas as tabelas, junto com o acesso

--- src/lib/middlewares/apiKey.js
+++ src/lib/middlewares/apiKey.js
@@ -1,8 +1,18 @@
 export const apiKey = (req, res, next) => {
     const key = req.headers['x-api-key'];
 
-    if (!key || key !== process.env.API_KEY) {
+    if (!key || key !== process.env.API_KEY && key !== process.env.ADMIN_KEY) {
         return res.status(401).json({ error: 'API Key inválida ou ausente.' });
+    }
+
+    next();
+};
+
+export const admKey = (req, res, next) => {
+    const key = req.headers['x-api-key'];
+
+    if (!key || key !== process.env.ADMIN_KEY) {
+        return res.status(401).json({ error: 'Admin Key inválida ou ausente.' });
     }
 
     next();
