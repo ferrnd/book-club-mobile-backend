1b879374c323a57ae3b8ff57e8540067c413f13b
ferrnd <sombarepam@gmail.com>
2026-05-10 13:35:21 -0300

    feat: adiciona middleware admKey na rota de atualização de livro para que apenas Possuintes da admKey possam atualizar a tabela

--- src/routes/livroRoute.js
+++ src/routes/livroRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import * as controller from "../controllers/livroController.js";
+import { admKey } from "../lib/middlewares/apiKey.js";
 
 const router = express.Router();
 
 router.get("/livro", controller.buscarTodos);
 router.get("/livro/:id", controller.buscarPorId);
-router.put("/livro/:id", controller.atualizar);
+router.put("/livro/:id", admKey, controller.atualizar);
 
 export default router;
