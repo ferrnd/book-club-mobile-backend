663ceb934f314fa757a3b9afeb962579556d561e
ferrnd <sombarepam@gmail.com>
2026-05-11 14:43:35 -0300

    feat: adiciona admKey middleware em PUT, DELETE e POST routes para maior segurança

--- src/routes/autorRoute.js
+++ src/routes/autorRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import * as controller from "../controllers/autorController.js";
+import { admKey } from '../lib/middlewares/apiKey.js';
 
 const router = express.Router();
 
 router.get("/autor", controller.buscarTodos);
 router.get("/autor/:id", controller.buscarPorId);
-router.put("/autor/:id", controller.atualizar);
+router.put("/autor/:id", admKey, controller.atualizar);
 
 export default router;
--- src/routes/projetoRoute.js
+++ src/routes/projetoRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import * as controller from "../controllers/projetoController.js";
+import { admKey } from '../lib/middlewares/apiKey.js';
 
 const router = express.Router();
 
 router.get("/projeto", controller.buscarTodos);
 router.get("/projeto/:id", controller.buscarPorId);
-router.put("/projeto/:id", controller.atualizar);
+router.put("/projeto/:id", admKey, controller.atualizar);
 
 export default router;
